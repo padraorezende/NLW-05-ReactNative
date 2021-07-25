361ea1fcdedca01f0a186221404dff704862f6e8
padraorezende <padraonxgalo@gmail.com>
2021-07-25 11:03:12 -0300

    Refatorando a pagina Welcome e importando fonte externa na aplicacao

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,58 +1,73 @@
 import React from "react"
-import {Text, SafeAreaView, Image, TouchableOpacity, StyleSheet} from "react-native"
-import wateringImg from '../../assets/watering.png';
-import { Button } from "../components/Button";
-import colors from "../styles/colors";
+import { Text, SafeAreaView, Image, TouchableOpacity, StyleSheet, Dimensions, View } from "react-native"
+import wateringImg from '../assets/watering.png'
+import colors from "../styles/colors"
+import fonts from "../styles/fonts"
+import { Feather } from "@expo/vector-icons"
 
-export function Welcome () {
+export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>Gerencie {'\n'} suas plantas {'\n'} de forma facil</Text>
-            <Image source={wateringImg} style={styles.image}/>
-            <Text style={styles.subtitle}>
-                Nao esqueça mais de regar suas plantas.
-                Nos cuidamos de lembrar voce sempre que precisar
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma facil
+                </Text>
+                <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+                <Text style={styles.subtitle}>
+                    Nao esqueça mais de regar suas plantas.
+                    Nos cuidamos de lembrar voce sempre que precisar
+                </Text>
 
-            <Button title=">"/>
+                <TouchableOpacity style={styles.button} activeOpacity={0.7} >
+                    <Feather name="chevron-right" style={styles.buttonIcon} />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
+        flex: 1,
+    },
+    wrapper:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: "space-between"
+        justifyContent: "space-around",
+        paddingHorizontal:20
     },
-    title:{
-        fontSize:32,
+    title: {
+        fontSize: 28,
         fontWeight: "bold",
         textAlign: "center",
         color: colors.heading,
-        marginTop:38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
-    subtitle:{
+    subtitle: {
         textAlign: "center",
         fontSize: 18,
-        paddingHorizontal:20,
+        paddingHorizontal: 20,
         color: colors.heading,
+        fontFamily: fonts.text
     },
-    button:{
+    image: {
+        height: Dimensions.get('window').width * 0.7
+    },
+    button: {
         backgroundColor: colors.green,
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 16,
-        marginBottom:10,
-        height:56,
-        width:56
-    }, 
-    image: {
-        width:292,
-        height:284
+        marginBottom: 10,
+        height: 56,
+        width: 56
     },
-    buttonText:{
+    buttonIcon: {
         color: colors.white,
-        fontSize:24
+        fontSize: 32
     }
 })
