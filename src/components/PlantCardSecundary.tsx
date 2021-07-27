49430146f844667e116295483c2d0050db7ebaeb
Padrao <padrao@dev01.dngbh.local>
2021-07-27 12:00:03 -0300

    Comecando a criar o componente Swipeable de deletar a planta salva e sua respectiva usabilidade

--- src/components/PlantCardSecundary.tsx
+++ src/components/PlantCardSecundary.tsx
@@ -1,66 +1,93 @@
 import React from "react"
-import {StyleSheet, Text, View} from "react-native"
-import {RectButton, RectButtonProps} from "react-native-gesture-handler"
+import { StyleSheet, Text, View } from "react-native"
+import { RectButton, RectButtonProps } from "react-native-gesture-handler"
+import Swipeable from "react-native-gesture-handler/Swipeable"
 import colors from "../styles/colors"
 import fonts from "../styles/fonts"
-import {SvgFromUri} from "react-native-svg"
+import { SvgFromUri } from "react-native-svg"
+import Animated from "react-native-reanimated"
+import { Feather } from "@expo/vector-icons"
 
-interface PlantProps extends RectButtonProps{
-    data:{
+interface PlantProps extends RectButtonProps {
+    data: {
         name: string,
         photo: string;
         hour: string
-    }
+    };
+    handleRemove: () => void;
 }
 
-export const PlantCardSecundary = ({data, ...rest}: PlantProps) => {
+export const PlantCardSecundary = ({ data,handleRemove, ...rest }: PlantProps) => {
     return (
-        <RectButton style={styles.container} {...rest}>
-            <SvgFromUri uri={data.photo} width={50} height={50}/>
-            <Text style={styles.title}>
-            {data.name}
-            </Text>
-            <View style={styles.details}>
-                <Text style={styles.timeLabel}>
-                    Regar as
+        <Swipeable overshootRight={false} renderRightActions={()=>(
+            <Animated.View>
+                <View>
+                    <RectButton style={styles.buttonRemove} onPress={handleRemove}>
+                        <Feather name="trash" size={32} color={colors.white}/>
+                    </RectButton>
+                </View>
+            </Animated.View>
+        )}>
+            <RectButton style={styles.container} {...rest}>
+                <SvgFromUri uri={data.photo} width={50} height={50} />
+                <Text style={styles.title}>
+                    {data.name}
                 </Text>
-                <Text style={styles.time}>
-                    {data.hour}
-                </Text>
-            </View>
-        </RectButton>
+                <View style={styles.details}>
+                    <Text style={styles.timeLabel}>
+                        Regar as
+                    </Text>
+                    <Text style={styles.time}>
+                        {data.hour}
+                    </Text>
+                </View>
+            </RectButton>
+        </Swipeable>
+
     )
 }
 const styles = StyleSheet.create({
-    container:{
+    container: {
         width: '100%',
-        paddingHorizontal:10,
+        paddingHorizontal: 10,
         paddingVertical: 25,
-        borderRadius:20,
+        borderRadius: 20,
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: colors.shape,
         marginVertical: 5
     },
-    title:{
-        flex:1,
-        marginLeft:10,
+    title: {
+        flex: 1,
+        marginLeft: 10,
         fontFamily: fonts.heading,
-        fontSize:17,
+        fontSize: 17,
         color: colors.heading
     },
-    details:{
-        alignItems:'flex-end'
+    details: {
+        alignItems: 'flex-end'
     },
-    timeLabel:{
-        fontSize:16,
-        fontFamily:fonts.text,
-        color:colors.body_light,
+    timeLabel: {
+        fontSize: 16,
+        fontFamily: fonts.text,
+        color: colors.body_light,
     },
-    time:{
-        marginTop:5,
-        fontSize:16,
-        fontFamily:fonts.heading,
-        color:colors.body_dark
+    time: {
+        marginTop: 5,
+        fontSize: 16,
+        fontFamily: fonts.heading,
+        color: colors.body_dark
+    },
+    buttonRemove:{
+        width: 100,
+        height: 85,
+        backgroundColor: colors.red,
+        marginTop: 15,
+        borderRadius:20,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
+        right: 20,
+        paddingLeft: 15
     }
 })
--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,22 +1,48 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Alert } from 'react-native'
 import colors from "../styles/colors";
 import { Header } from "../components/Header";
 import waterDrop from "../assets/waterdrop.png"
 import { FlatList } from "react-native-gesture-handler";
 import { useState } from "react";
-import { loadPlant, PlantProps } from "../libs/storage";
+import { loadPlant, PlantProps, StoragePlantProps } from "../libs/storage";
 import { useEffect } from "react";
 import { formatDistance } from "date-fns";
 import { pt } from "date-fns/locale";
 import fonts from "../styles/fonts";
 import { PlantCardSecundary } from "../components/PlantCardSecundary";
 import { Load } from "../components/Load";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function MyPlants() {
     const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [nextWatered, setnextWatered] = useState<string>();
+
+    function handleRemove(plant: PlantProps){
+        Alert.alert('Remover', `Deseja remover a ${plant.name}`,[
+            {
+                text: "Nao", 
+                style: "cancel"
+            },
+            {
+                text: "Sim",
+                onPress: async () => {
+                    try{
+                        const data = await AsyncStorage.getItem('@plantmanager:plants');
+                        const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+                        delete plants[plant.id];
+
+                        await AsyncStorage.setItem('@plantmanager:plants', JSON.stringify(plants));
+
+                    }catch(error){
+
+                    }
+                }
+            }
+        ])
+    }
 
     useEffect(()=>{
         async function loadStorageData() {
@@ -59,7 +85,7 @@ export function MyPlants() {
 
                 <FlatList data={myPlants} keyExtractor={(item)=> String(item.id)} 
                 renderItem={({item})=> (
-                    <PlantCardSecundary data={item}/>
+                    <PlantCardSecundary data={item} handleRemove={()=> {handleRemove(item)}}/>
                 )} 
                 showsVerticalScrollIndicator={false} contentContainerStyle={{flex: 1}}/>
             </View>
